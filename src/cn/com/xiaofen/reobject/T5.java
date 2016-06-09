a556e727cb7471ed6a6610fdcf2cef078db49670
xiaofen <247690790@qq.com>
2016-06-09 20:23:22 +0800

    由方法返回Class对象

--- src/cn/com/xiaofen/reobject/T5.java
+++ src/cn/com/xiaofen/reobject/T5.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 public class T5 {
 	@Test
 	public void test(){
+		/*存在几种能够返回Class对象的API，只不过只能在已知的Class对象中调用*/
 		/*返回父类的Class对象*/
 		Class c = javax.swing.JButton.class.getSuperclass();
 		/*返回Class兑现的所有公共类、接口、枚举、成员，包括继承的成员*/
@@ -14,7 +15,6 @@ public class T5 {
 		Class<?>[] e = Character.class.getDeclaredClasses();
 		/*返回当前类的申明Class对象*/
 		Class f=Character.class.getDeclaringClass();
-			
 		//java.lang.reflect.Field.getDeclaringClass()
 		//java.lang.reflect.Method.getDeclaringClass()
 		//java.lang.reflect.Constructor.getDeclaringClass()
